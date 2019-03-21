3bdad0c591ec5b2902eea34f65c5d4d8ece2d56a
lml2014 <982905676@qq.com>
2019-03-21 21:17:49 +0800

    support druid

---
+++ mybatis/src/test/java/com/lml/boot/mybatis/ApplicationTest.java
@@ -0,0 +1,12 @@
+package com.lml.boot.mybatis;
+
+import org.junit.Test;
+
+
+public class ApplicationTest {
+
+    @Test
+    public void test() throws Exception {
+        System.out.println("application");
+    }
+}
---
+++ mybatis/src/test/java/com/lml/boot/mybatis/DruidConfigTest.java
@@ -0,0 +1,31 @@
+package com.lml.boot.mybatis;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DruidConfigTest {
+
+    @Resource
+    DataSource dataSource;
+
+    @Test
+    public void testDruid() throws Exception {
+        System.out.println("数据源>>>>>>" + dataSource.getClass());
+        Connection connection = dataSource.getConnection();
+        System.out.println("连接>>>>>>>>>" + connection);
+        System.out.println("连接地址>>>>>" + connection.getMetaData().getURL());
+        DruidDataSource druidDataSource = (DruidDataSource) dataSource;
+        System.out.println("druidDataSource 数据源最大连接数：" + druidDataSource.getMaxActive());
+        System.out.println("druidDataSource 数据源初始化连接数：" + druidDataSource.getInitialSize());
+        connection.close();
+    }
+}
